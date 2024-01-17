c46e6c63105b1471e471dfbc7736c4f35ed9e685
Anvith <819178@apps.district196.org>
2024-01-16 22:32:00 -0600

    added collapsible + logo

--- scouter/index.js
+++ scouter/index.js
@@ -77,5 +77,21 @@ function clearForm() {
     }
 }
 
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var section = this.nextElementSibling;
+    if (section.style.display === "block") {
+      section.style.display = "none";
+    } else {
+      section.style.display = "block";
+    }
+  });
+}
+
 document.getElementById("endOfForm").addEventListener('click', submitResults);
 document.getElementById("clearForm").addEventListener('click', clearForm);
