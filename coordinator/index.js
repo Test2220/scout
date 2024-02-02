8cda5e49fe42ca443c2ab10fcfc2a22f303ba5c0
ethankrech <ethankrech@protonmail.com>
2024-02-01 19:52:54 -0600

    k

--- coordinator/index.js
+++ coordinator/index.js
@@ -4,6 +4,43 @@
 // 	sendFunction()
 // }
 
+import http from 'http'
+import fs from 'fs'
+
+http
+    .createServer((request, response) => {
+        fs.readFile(`.${request.url}`, (err, data) => {
+            if (err) {
+                response.writeHeader(404, {
+                    'Content-Type': 'text/plain'
+                })
+                response.write('404 Not Found')
+                response.end()
+                return
+            }
+
+            if (request.url.endsWith('.html')) {
+                response.writeHeader(200, {
+                    'Content-Type': 'text/html'
+                })
+            }
+
+            if (request.url.endsWith('.js')) {
+                response.writeHeader(200, {
+                    'Content-Type': 'application/javascript'
+                })
+            }
+
+            response.write(data)
+            response.end()
+        })
+    })
+    .listen(PORT)
+
+const PORT = 3000
+
+
+
 ////////////////////////////////////////////////////////////////////////
 
 
