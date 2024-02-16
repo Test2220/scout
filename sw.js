81888f03ea1989235f537af4ea33adb326001690
ethankrech <ethankrech@protonmail.com>
2024-02-15 20:21:01 -0600

    A

--- sw.js
+++ sw.js
@@ -1,47 +1,47 @@
-const cacheName = 'v1'
+// const cacheName = 'v1'
 
-const cacheAssets = [
-    'index.html',
-    'index.js',
-    'styles.css',
-    'README.md',
-]
+// const cacheAssets = [
+//     'index.html',
+//     'index.js',
+//     'styles.css',
+//     'README.md',
+// ]
 
-self.addEventListener('install', e => {
-    console.log('Service worker installed')
+// self.addEventListener('install', e => {
+//     console.log('Service worker installed')
 
-    e.waitUntil(
-        caches
-            .open(cacheName)
-            .then(cache => {
-                console.log('Service worker caching files')
-                cache.addAll(cacheAssets)
-            })
-        .then(() => self.skipWaiting())
-    )
-})
+//     e.waitUntil(
+//         caches
+//             .open(cacheName)
+//             .then(cache => {
+//                 console.log('Service worker caching files')
+//                 cache.addAll(cacheAssets)
+//             })
+//         .then(() => self.skipWaiting())
+//     )
+// })
 
-self.addEventListener('activate', e => {
-    console.log('Service worker activated')
+// self.addEventListener('activate', e => {
+//     console.log('Service worker activated')
 
-    e.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => {
-                    if (cache !== cacheName) {
-                        console.log('Service worker removing old caches')
-                        return caches.delete(cache)
-                    }
-                })
-            )
-        })
-    )
-})
+//     e.waitUntil(
+//         caches.keys().then(cacheNames => {
+//             return Promise.all(
+//                 cacheNames.map(cache => {
+//                     if (cache !== cacheName) {
+//                         console.log('Service worker removing old caches')
+//                         return caches.delete(cache)
+//                     }
+//                 })
+//             )
+//         })
+//     )
+// })
 
-self.addEventListener('fetch', e => {
-    console.log('Service worker fetching')
+// self.addEventListener('fetch', e => {
+//     console.log('Service worker fetching')
 
-    e.respondWith(
-        fetch(e.request).catch(() => caches.match(e.request))
-    )
-})
+//     e.respondWith(
+//         fetch(e.request).catch(() => caches.match(e.request))
+//     )
+// })
