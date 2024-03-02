1819e229554eb4e0c25b56f2123fd83b4ad8c0f9
ethankrech <ethankrech@protonmail.com>
2024-03-02 13:41:16 -0600

    Links?

--- sw.js
+++ sw.js
@@ -3,22 +3,22 @@ const version = "v1"
 const cacheName = `scout-2220-${version}`
 
 const appResources = [
-    "/",
-    "/index.html",
-    "/style.css",
-    "/logo.png",
-    "/README.md",
-    "/index.js",
-    "/manifest.json",
-    "/scouter/button.js",
-    "/scouter/index.html",
-    "/scouter/index.js",
-    "/scouter/qr.js",
-    "/scouter/styles.css",
-    "/scanner/index.html",
-    "/scanner/index.js",
-    "/scanner/styles.css",
-    "/scanner/scanner.js"
+    "https://scout-2220.vercel.app/",
+    "https://scout-2220.vercel.app/index.html",
+    "https://scout-2220.vercel.app/style.css",
+    "https://scout-2220.vercel.app/logo.png",
+    "https://scout-2220.vercel.app/README.md",
+    "https://scout-2220.vercel.app/index.js",
+    "https://scout-2220.vercel.app/manifest.json",
+    "https://scout-2220.vercel.app/scouter/button.js",
+    "https://scout-2220.vercel.app/scouter/index.html",
+    "https://scout-2220.vercel.app/scouter/index.js",
+    "https://scout-2220.vercel.app/scouter/qr.js",
+    "https://scout-2220.vercel.app/scouter/styles.css",
+    "https://scout-2220.vercel.app/scanner/index.html",
+    "https://scout-2220.vercel.app/scanner/index.js",
+    "https://scout-2220.vercel.app/scanner/styles.css",
+    "https://scout-2220.vercel.app/scanner/scanner.js"
 ]
   
 self.addEventListener("install", (event) => {
