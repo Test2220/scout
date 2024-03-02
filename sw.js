3599a254c6b64deef471970b7c06eea0f296dc20
ethankrech <ethankrech@protonmail.com>
2024-03-02 11:07:22 -0600

    A

--- sw.js
+++ sw.js
@@ -1,47 +1,74 @@
-// const cacheName = 'v1'
+const version = "v1"
 
-// const cacheAssets = [
-//     'index.html',
-//     'index.js',
-//     'styles.css',
-//     'README.md',
-// ]
+const cacheName = `scout-2220-${version}`
 
-// self.addEventListener('install', e => {
-//     console.log('Service worker installed')
-
-//     e.waitUntil(
-//         caches
-//             .open(cacheName)
-//             .then(cache => {
-//                 console.log('Service worker caching files')
-//                 cache.addAll(cacheAssets)
-//             })
-//         .then(() => self.skipWaiting())
-//     )
-// })
-
-// self.addEventListener('activate', e => {
-//     console.log('Service worker activated')
-
-//     e.waitUntil(
-//         caches.keys().then(cacheNames => {
-//             return Promise.all(
-//                 cacheNames.map(cache => {
-//                     if (cache !== cacheName) {
-//                         console.log('Service worker removing old caches')
-//                         return caches.delete(cache)
-//                     }
-//                 })
-//             )
-//         })
-//     )
-// })
-
-// self.addEventListener('fetch', e => {
-//     console.log('Service worker fetching')
-
-//     e.respondWith(
-//         fetch(e.request).catch(() => caches.match(e.request))
-//     )
-// })
+const appResources = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/logo.png",
+    "/README.md",
+    "/index.js",
+]
+  
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+      (async () => {
+        const cache = await caches.open(cacheName)
+        cache.addAll(appResources)
+      })(),
+    )
+  })
+  
+  self.addEventListener("activate", (event) => {
+    event.waitUntil(
+      (async () => {
+        const names = await caches.keys();
+        await Promise.all(
+          names.map((name) => {
+            if (name !== cacheName) {
+              return caches.delete(name);
+            }
+          }),
+        );
+        await clients.claim();
+      })(),
+    );
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    // when seeking an HTML page
+    if (event.request.mode === "navigate") {
+      // Return to the index.html page
+      event.respondWith(caches.match("/"));
+      return;
+    }
+  
+    // For every other request type
+    event.respondWith(
+      (async () => {
+        const cache = await caches.open(cacheName);
+        const cachedResponse = await cache.match(event.request.url);
+        if (cachedResponse) {
+          // Return the cached response if it's available.
+          return cachedResponse;
+        }
+        // Respond with a HTTP 404 response status.
+        return new Response(null, { status: 404 });
+      })(),
+    );
+  });
+  
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js").then(
+      (registration) => {
+        console.log("Service worker registration successful:", registration);
+      },
+      (error) => {
+        console.error(`Service worker registration failed: ${error}`);
+      },
+    );
+  } else {
+    console.error("Service workers are not supported.");
+  }
+  
